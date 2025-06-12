19b4cb6d3ff8f31f195b6e6ff7c234a92847387b
yhonder710 <agueropibe1@gmail.com>
2025-06-12 11:12:50 -0400

    feat: nav maquetado

--- src/components/organismos/Naybar.tsx
+++ src/components/organismos/Naybar.tsx
@@ -1,7 +1,28 @@
 export function Naybar() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <nav className="flex justify-between items-center px-2.5">
+      <strong>Inicio</strong>
+      <ul className="flex gap-5">
+        {
+          link.map(link => (
+            <li key={link.name}>
+              <a href={link.to}>{link.name}</a>
+            </li>
+          ))
+        }
+      </ul>
+      <div className="flex gap-3">
+        <button>Ws</button>
+        <button>Email</button>
+      </div>
+    </nav>
   )
 }
+
+const link = [
+  { name: "Quienes somos", to: "quienes-somos", },
+  { name: "Que ofrecemos", to: "que-ofrecemos", },
+  { name: "Ventajas del servicio", to: "ventaja-del-servicion", },
+  { name: "Planes del servicio", to: "planes-del-servicio", },
+  { name: "Contactanos", to: "contactanos", }
+]
