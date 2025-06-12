187ca5fa7a5586d34e3efd6b9f67ef8a2f067503
yhonder710 <agueropibe1@gmail.com>
2025-06-12 12:29:29 -0400

    feact: btn contact creado

--- src/components/atomos/BtnPrimary.tsx
+++ src/components/atomos/BtnPrimary.tsx
@@ -1,12 +1,16 @@
-import { FaSquareWhatsapp } from "react-icons/fa6";
+import type { ReactNode } from "react"
 
+interface Props {
+  icon: ReactNode
+  text: string
+}
 
-export function BtnPrimary() {
+export function BtnPrimary({ icon, text }: Props) {
   return (
-    <button className="border-1 p-1 rounded-[5px] text-white">
+    <button className="border-1 p-1 px-2 rounded-[5px] text-[#0AFFFF]">
       <a href="#" className="flex items-center gap-1">
-        <FaSquareWhatsapp size={30} />
-        <strong>Whatsapp</strong>
+        {icon}
+        <strong>{text}</strong>
       </a>
     </button>
   )
--- src/components/organismos/Naybar.tsx
+++ src/components/organismos/Naybar.tsx
@@ -1,6 +1,17 @@
 import logo from '../../assets/diseño-de-logotipo-con-letra-k.webp'
 import { BtnPrimary } from '../atomos/BtnPrimary'
 
+import { FaSquareWhatsapp } from "react-icons/fa6";
+import { BiLogoGmail } from "react-icons/bi";
+
+import { IoPersonSharp } from "react-icons/io5";
+import { FaHandHoldingUsd } from "react-icons/fa";
+import { AiFillTrophy } from "react-icons/ai";
+import { AiFillCreditCard } from "react-icons/ai";
+import { MdOutlineConnectWithoutContact } from "react-icons/md";
+
+
+
 export function Naybar() {
   return (
     <nav className="flex justify-between items-center px-2.5 py-1 bg-[#0E6F7C]">
@@ -9,27 +20,27 @@ export function Naybar() {
           <img src={logo} alt="Logo" className=' w-10 h-auto object-cover rounded-[6px]' />
         </a>
       </picture>
-      <ul className="flex gap-5">
+      <ul className="flex gap-5 text-[#e9e6e6]">
         {
           link.map(link => (
             <li key={link.name}>
-              <a href={link.to}>{link.name}</a>
+              <a href={link.to} className='text-[20px] flex items-center gap-1 justify-center'>{link.icon} {link.name}</a>
             </li>
           ))
         }
       </ul>
       <div className="flex gap-3">
-        <BtnPrimary />
-        <button>Email</button>
+        <BtnPrimary icon={<FaSquareWhatsapp size={32} />} text='Whatsapp' />
+        <BtnPrimary icon={<BiLogoGmail size={32} />} text='Email' />
       </div>
     </nav>
   )
 }
 
 const link = [
-  { name: "Quienes somos", to: "quienes-somos", },
-  { name: "Que ofrecemos", to: "que-ofrecemos", },
-  { name: "Ventajas del servicio", to: "ventaja-del-servicion", },
-  { name: "Planes del servicio", to: "planes-del-servicio", },
-  { name: "Contactanos", to: "contactanos", }
+  { name: "Quienes somos", to: "#", icon: <IoPersonSharp /> },
+  { name: "Que ofrecemos", to: "#", icon: <FaHandHoldingUsd /> },
+  { name: "Ventajas del servicio", to: "#", icon: <AiFillTrophy /> },
+  { name: "Planes del servicio", to: "#", icon: <AiFillCreditCard /> },
+  { name: "Contactanos", to: "#", icon: <MdOutlineConnectWithoutContact /> }
 ]
